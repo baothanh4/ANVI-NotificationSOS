3e32757689df004c578f47519c380932fd157530
baothanh4 <taobaothanh365@gmail.com>
2026-05-12 22:53:08 +0700

    Fix

--- src/main/java/com/example/anvisos/config/WebMvcConfig.java
+++ src/main/java/com/example/anvisos/config/WebMvcConfig.java
@@ -16,6 +16,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(roleInterceptor)
                 .addPathPatterns("/api/**")
-                .excludePathPatterns("/api/auth/**", "/api/users/public-info/**", "/api/qr/cards/*/public-info");
-    }
 }
